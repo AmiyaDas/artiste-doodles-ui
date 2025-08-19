d4af8ea65224f056ba133a2797f7a49cd9b02b70
Amiya Ranjan Das <ranjandass13@gmail.com>
2025-08-20 01:14:59 +0530

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { FiBarChart } from "react-icons/fi";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaChevronLeft } from "react-icons/fa";
+import { useNavigate, Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+const Header = ({
+  showNavBack = false,
+  title = "",
+  showIcons = false,
+  isAppTitle = false,
+}) => {
+  //   const navigate = useNavigate();
+  //   const { t } = useTranslation();
+  return (
+    <header className="header">
+      <div className="left">
+        {/* {showNavBack && <FaChevronLeft onClick={() => navigate(-1)} />} */}
+        <FaChevronLeft />
+      </div>
+
+      <div className={"center title" + (isAppTitle ? " app-title" : "")}>
+        {"Artiste Doodles"}
+      </div>
+      <div className="right">
+        {showIcons && (
+          <div className="icons">
+            <Link to="/analytics">
+              <FiBarChart />
+            </Link>
+            <Link to="/settings">
+              <IoSettingsOutline />
+            </Link>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      <h1>Welcome to Artiste Doodles</h1>
+      <p>Your one-stop solution for creative doodling!</p>
+    </div>
+  );
+};
+
+export default Home;
